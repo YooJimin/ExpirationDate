e3b34f166fe21b77ef3fb1841011d0f51ce02986
Jimin Yoo <jimin.yoo@navercorp.com>
2020-10-07 18:39:08 +0900

    [LocationPopup] UX 개선.

--- ExpirationDate/Location/ViewControllers/JMYLocationPopupViewController.swift
+++ ExpirationDate/Location/ViewControllers/JMYLocationPopupViewController.swift
@@ -13,11 +13,18 @@ class JMYLocationPopupViewController: JMYViewController {
     var originalName: String?
     var completion: (() -> Void)?
     
+    var filteredName: String {
+        return self.nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+    }
+    
     @IBOutlet weak var popupContainerView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var okButton: UIButton!
+    
+    @IBOutlet weak var popupContainerViewCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var popupContainerViewBottomConstraint: NSLayoutConstraint!
     
     init(name: String? = nil, completion: (() -> Void)?) {
         super.init(nibName: nil, bundle: nil)
@@ -30,19 +37,69 @@ class JMYLocationPopupViewController: JMYViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK:- View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.registerNotifications()
         self.setupViews()
     }
-
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK:- Notifications
+    
+    func registerNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc
+    func keyboardWillShow(_ notification: Notification) {
+        let popupViewMaxY = self.popupContainerView.frame.maxY
+        if let keyboard = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            popupViewMaxY + 20 > keyboard.minY {
+            self.popupContainerViewCenterYConstraint.constant = popupViewMaxY + 20 - keyboard.minY
+        }
+        
+        UIView.animate(withDuration: notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval ?? 0.25, delay: 0, options: .curveEaseInOut, animations: { [weak self] in
+            self?.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @objc
+    func keyboardWillHide(_ notification: Notification) {
+        self.popupContainerViewCenterYConstraint.constant = 0
+        UIView.animate(withDuration: notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval ?? 0.25, delay: 0, options: .curveEaseInOut, animations: { [weak self] in
+            self?.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    // MARK:- Setups
+    
     func setupViews() {
         self.popupContainerView.clipsToBounds = true
         self.popupContainerView.layer.cornerRadius = 15.0
         self.cancelButton.layer.cornerRadius = 5.0
         self.okButton.layer.cornerRadius = 5.0
+        
+        self.nameTextField.text = self.originalName
+        self.nameTextField.becomeFirstResponder()
+        
+        self.updateControls()
     }
-
+    
+    func updateControls() {
+        let isEnabled = !self.filteredName.isEmpty
+        self.okButton.backgroundColor = isEnabled ? .orange : .gray
+        self.okButton.isEnabled = isEnabled
+    }
+    
+    // MARK:- actions
+    
     @IBAction func touchUpInsideCancelButton(_ sender: Any) {
         
         self.dismiss(animated: true, completion: nil)
@@ -51,7 +108,7 @@ class JMYLocationPopupViewController: JMYViewController {
     
     @IBAction func touchUpInsideOkButton(_ sender: Any) {
         
-        if let name = self.nameTextField.text, name.count > 0 {
+        if let name = self.nameTextField.text, !name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             
             do {
                 try DataBaseManager.shared.createLocation(name: name)
@@ -66,5 +123,18 @@ class JMYLocationPopupViewController: JMYViewController {
             
         }
         
+    }
+    
+    @IBAction func tapGesture(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func textFieldValueChanged(_ textField: UITextField) {
+        
+        guard textField === self.nameTextField, textField.text != self.filteredName else { return }
+        
+        textField.text = self.filteredName
+        
+        self.updateControls()
     }
 }
